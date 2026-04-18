9de76286b55517794dcbd5318d6754e37ca75dea
ydah <t.yudai92@gmail.com>
2026-04-18 17:46:33 +0900

    Honor serialize addon options

--- spec/addons/serialize_spec.rb
+++ spec/addons/serialize_spec.rb
@@ -34,6 +34,34 @@ RSpec.describe RTerm::Addon::Serialize do
       replayed_line = term2.buffer.active.get_line(0).to_string
       expect(replayed_line).to eq(original_line)
     end
+
+    it "includes requested scrollback lines" do
+      small = RTerm::Terminal.new(cols: 10, rows: 2, scrollback: 3)
+      small_serializer = described_class.new
+      small.load_addon(small_serializer)
+      4.times { |index| small.writeln("line#{index}") }
+
+      result = small_serializer.serialize(scrollback: 2)
+
+      expect(result).to include("line1")
+      expect(result).to include("line3")
+    end
+
+    it "can exclude the alternate buffer" do
+      terminal.write("normal")
+      terminal.write("\e[?1049h")
+      terminal.write("alt")
+
+      expect(serializer.serialize(exclude_alt_buffer: true)).to include("normal")
+      expect(serializer.serialize(exclude_alt_buffer: true)).not_to include("alt")
+    end
+
+    it "can exclude mode sequences" do
+      terminal.write("\e[?7l")
+
+      expect(serializer.serialize).to include("\e[?7l")
+      expect(serializer.serialize(exclude_modes: true)).not_to include("\e[?7l")
+    end
   end
 
   describe "#serialize_as_html" do
